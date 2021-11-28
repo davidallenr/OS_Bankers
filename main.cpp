5f69904107e7631d61713e02980b1a263b5d6a7e
David <davidallenr@gmail.com>
2021-11-28 11:34:33 -0500

    refactor code added need table function

--- main.cpp
+++ main.cpp
@@ -8,17 +8,14 @@ int find_process_count(std::vector<std::string>);
 int find_matrix_beginning(std::vector<std::string>, std::string);
 std::vector<std::vector<int>> populate_2d_vector(std::vector<std::string>, int, int, int);
 std::vector<int> populate_available(std::vector<std::string>);
+std::vector<std::vector<int>> return_need_table(std::vector<std::vector<int>>, std::vector<std::vector<int>>, int);
+void display_information(std::vector<std::string>, int, int, int, int, int, int, std::vector<std::vector<int>>, std::vector<std::vector<int>>, std::vector<int>);
+
 
 int main() {
     std::ifstream file;
     std::string line;
     std::vector<std::string> file_contents;
-    std::vector<int> available_table;
-    std::vector<std::vector<int>> allocation_table;
-    std::vector<std::vector<int>> max_table;
-    std::vector<std::vector<int>> need_table;
-    int number_of_processes = 0;
-    int number_of_resources = 0;
 
     file.open("data.txt");
 
@@ -31,44 +28,26 @@ int main() {
 
     file.close();
 
-    number_of_resources = find_resource_count(file_contents);
-    number_of_processes = find_process_count(file_contents);
+    int number_of_resources = find_resource_count(file_contents);
+    int number_of_processes = find_process_count(file_contents);
     int allocation_matrix_start = find_matrix_beginning(file_contents, "Allocation:");
     int allocation_matrix_end = allocation_matrix_start + number_of_processes;
     int max_matrix_start = find_matrix_beginning(file_contents, "Max:");
     int max_matrix_end = max_matrix_start + number_of_processes;
-    available_table = populate_available(file_contents);
-    allocation_table = populate_2d_vector(file_contents, allocation_matrix_start, allocation_matrix_end, number_of_resources);
-    max_table = populate_2d_vector(file_contents, max_matrix_start, max_matrix_end, number_of_resources);
+    std::vector<int> available_table = populate_available(file_contents);
+    std::vector<std::vector<int>> allocation_table = populate_2d_vector(file_contents, allocation_matrix_start, allocation_matrix_end, number_of_resources);
+    std::vector<std::vector<int>> max_table = populate_2d_vector(file_contents, max_matrix_start, max_matrix_end, number_of_resources);
+    std::vector<std::vector<int>> need_table = return_need_table(max_table, allocation_table, number_of_resources);
 
-
-    std::cout << "------- BEGIN Information -------------\n";
-    std::cout << "Number of Processes: " << number_of_processes << '\n';
-    std::cout << "Number of Resources: " << number_of_resources << '\n';
-    std::cout << "Allocation Start: " << allocation_matrix_start << '\n';
-    std::cout << "Allocation End: " << allocation_matrix_end << '\n';
-    std::cout << "Max Start: " << max_matrix_start << '\n';
-    std::cout << "Max End: " << max_matrix_end << '\n';
-    std::cout << "Allocation Table Size: " << allocation_table.size() << '\n';
-    std::cout << "Max Table Size: " << max_table.size() << '\n';
-    std::cout << "\n";
-    std::cout << "--- AVAIL TABLE ---\n";
-    std::cout << "    ";
-    for (auto i = 0; i < available_table.size(); ++i) {
-        std::cout << available_table[i] << " ";
+    display_information(file_contents, number_of_processes, number_of_resources, allocation_matrix_start, allocation_matrix_end, max_matrix_start, max_matrix_end, allocation_table, max_table, available_table);
+    std::cout << need_table.size();
+    /*
+    for (int i = 0; i < need_table.size(); ++i) {
+        for (int j = 0; j < need_table[i].size(); ++j) {
+            std::cout << need_table[i][j] << ' ';
+        }
     }
-    std::cout << "\n";
-    std::cout << "--- ALLOC MATRIX ---\n";
-    for (auto i = allocation_matrix_start; i < allocation_matrix_end; ++i) {
-        std::cout << "    " << file_contents[i] << std::endl;
-    }
-    std::cout << "\n";
-    std::cout << "--- MAX MATRIX ---\n";
-    for (auto i = max_matrix_start; i < max_matrix_end; ++i) {
-        std::cout << "    " << file_contents[i] << std::endl;
-    }
-    std::cout << "-------- END Information ------------\n ";
-
+    */
     return 0;
 }
 
@@ -162,4 +141,57 @@ std::vector<int> populate_available(std::vector<std::string> to_process) {
     }
 
     return temp;
+}
+
+// INPUT: Takes (2) std::vector<std::vector<int>> and returns std::vector<std::vector<int>> based on LHS - RHS.
+// RETURN: 2D Need vector table
+std::vector<std::vector<int>> return_need_table(std::vector<std::vector<int>> max, std::vector<std::vector<int>> available, int resource_num) {
+    std::vector<int> temp;
+    std::vector<std::vector<int>> temp_2d;
+
+    for (auto i = 0; i < max.size(); ++i) {
+        int count = 0;
+        for (auto j = 0; j < max[i].size(); ++j) {
+            ++count;
+            temp.push_back(max[i][j] - available[i][j]);
+
+            if ((count % resource_num) == 0) {
+                temp_2d.push_back(temp);
+                temp.clear();
+                count = 0;
+            }
+
+        }
+    }
+
+    return temp_2d;
+}
+
+void display_information(std::vector<std::string> to_process, int process_num, int resource_num, int alloc_start, int alloc_end, int max_start, int max_end, std::vector<std::vector<int>> alloc_table, std::vector<std::vector<int>> mx_table, std::vector<int> avail_table  ) {
+    std::cout << "------- BEGIN Information -------------\n";
+    std::cout << "Number of Processes: " << process_num << '\n';
+    std::cout << "Number of Resources: " << resource_num << '\n';
+    std::cout << "Allocation Start: " << alloc_start << '\n';
+    std::cout << "Allocation End: " << alloc_end << '\n';
+    std::cout << "Max Start: " << max_start << '\n';
+    std::cout << "Max End: " << max_end << '\n';
+    std::cout << "Allocation Table Size: " << alloc_table.size() << '\n';
+    std::cout << "Max Table Size: " << mx_table.size() << '\n';
+    std::cout << "\n";
+    std::cout << "--- AVAIL TABLE ---\n";
+    std::cout << "    ";
+    for (auto i = 0; i < avail_table.size(); ++i) {
+        std::cout << avail_table[i] << " ";
+    }
+    std::cout << "\n";
+    std::cout << "--- ALLOC MATRIX ---\n";
+    for (auto i = alloc_start; i < alloc_end; ++i) {
+        std::cout << "    " << to_process[i] << std::endl;
+    }
+    std::cout << "\n";
+    std::cout << "--- MAX MATRIX ---\n";
+    for (auto i = max_start; i < max_end; ++i) {
+        std::cout << "    " << to_process[i] << std::endl;
+    }
+    std::cout << "-------- END Information ------------\n ";
 }
